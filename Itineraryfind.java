e4685bd551935c8570123f8676ba01a834ee1905
SAURABH KUMAR <147742073+skkushwah03@users.noreply.github.com>
2025-12-10 15:45:11 +0530

    Create Itineraryfind.java

---
+++ Itineraryfind.java
@@ -0,0 +1,33 @@
+package _20_Hashing;
+import java.util.*;
+public class Itineraryfind {
+    public static String getStart(HashMap<String,String> tickets){
+        HashMap<String,String>revMap=new HashMap<>();
+        for(String key:tickets.keySet()){
+            revMap.put(tickets.get(key),key);
+        }
+        for(String key:tickets.keySet()){
+        if(!revMap.containsKey(key)){
+            return key;
+        }
+    }
+    return null;
+    
+    }
+    public static void main(String[] args) {
+        HashMap<String,String>tickets=new HashMap<>();
+        tickets.put("chennai", "Bengaluru");
+        tickets.put("Mumbai","Delhi");
+        tickets.put("Goa","Chennai");
+        tickets.put("Delhi","Goa");
+
+        String start=getStart(tickets);
+        System.out.println(start);
+        for(String key:tickets.keySet()){
+            System.out.print("->"+tickets.get(start));
+            start=tickets.get(start);
+        }
+        System.out.println();
+
+    }
+}
